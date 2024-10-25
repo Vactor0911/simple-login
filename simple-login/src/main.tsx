929f4b05edfe14ab80dc3d910fcf1bca88d2bcea
Vactor0911 <vactor0911@gmail.com>
2024-10-25 23:25:39 +0900

    Added pages

--- simple-login/src/main.tsx
+++ simple-login/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login, Register } from './assets/pages'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
