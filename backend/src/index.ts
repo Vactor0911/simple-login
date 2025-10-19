c986f708835caf6374355a87b7657f4dfefb89cb
Vactor0911 <vactor0911@gmail.com>
2025-10-20 03:34:44 +0900

    cors 보호 추가

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,22 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { config } from "./config";
 import authRoutes from "./routes/auth.routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://vactor0911.github.io"
+        : `http://localhost:8080`,
+    credentials: true, // 쿠키 전송 허용
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // API
 app.use("/api/auth", authRoutes);
